27a3f717eb38cb1ccecdce8fb7f0120b206d2793
peppapig13132 <peppapig13132@outlook.com>
2024-07-16 10:45:43 +0200

    You Received a Gift Card

--- templates/emails/gift-card-received-email.php
+++ templates/emails/gift-card-received-email.php
@@ -1,6 +1,6 @@
 <div style="max-width: 400px; margin: auto; padding: 0 20px">
   <p style="text-align: center; font-size: 1.5rem; font-weight: 600;">
-    Dear Yuval Silver,
+    Dear {{RECEIVER_NAME}},
   </p>
   <div style="margin: 30px 0; text-align: center;">
     <img src="https://raw.githubusercontent.com/peppapig13132/GiftCardify-for-WooCommerce/development/assets/images/others/Gift_box.png" style="width: 80px;" alt="Gift box">
@@ -8,8 +8,8 @@
   </div>
   <p style="text-align: center;">
     YOU HAVE RECEIVED A GIFT CARD FROM: <br />
-    Keren Naftali, <br /><br />
-    Good luck, health and happiness for the new year
+    {{SENDER_NAME}}, <br /><br />
+    {{GIFT_MESSAGE}}
   </p>
 
   <p style="text-align: center; margin-top: 60px; margin-bottom: 0px; font-size: 1.75rem;">LISTEN TO YOUR SOUL</p>
@@ -24,14 +24,14 @@
       <!-- <img src="../../assets/images/others/LISTEN-TO-YOUR-SOUL-WHITE.png" style="max-width: 80%;" alt="LISTEN TO YOUR SOUL"> -->
     </div>
     <div style="max-width: 80%; margin: auto; padding: 5px 10px; background-color: #A7D014; color: #222;">
-      <p style="text-align: center; font-size: 1.25rem; font-weight: 1.25rem; margin: 0;">Gift Card Code: LTYS-300-24</p>
+      <p style="text-align: center; font-size: 1.25rem; font-weight: 1.25rem; margin: 0;">Gift Card Code: {{GIFT_CARD_CODE}}</p>
     </div>
   </div>
 
   <p style="text-align: center; margin-top: 50px;">
     Gift Card can only be used at ltysoul.com. <br />
     Effective for 1 year. <br />
-    Valid until Sep 1, 2025 <br />
+    Valid until {{AVAILABLE_DATE}} <br />
     For personal use only
   </p>
 
@@ -71,22 +71,22 @@
         <td style="padding: 10px 0; text-align: right;">
           <div style="display: inline-block; margin-left: 5px; width: 20px; vertical-align: middle;">
             <a href="https://www.facebook.com/" style="text-decoration: none;" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
+              <img src="{{ASSETS_PATH}}/assets/images/social-icons/facebook.svg" alt="fb">
             </a>
           </div>
           <div style="display: inline-block; margin-left: 5px; width: 20px; vertical-align: middle;">
             <a href="https://instagram.com/" style="text-decoration: none;" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+              <img src="{{ASSETS_PATH}}/assets/images/social-icons/instagram.svg" alt="insta">
             </a>
           </div>
           <div style="display: inline-block; margin-left: 5px; width: 20px; vertical-align: middle;">
             <a href="https://www.tiktok.com/" style="text-decoration: none;" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z"/></svg>
+              <img src="{{ASSETS_PATH}}/assets/images/social-icons/tiktok.svg" alt="ttk">
             </a>
           </div>
           <div style="display: inline-block; margin-left: 5px; width: 20px; vertical-align: middle;">
             <a href="https://twitter.com/" style="text-decoration: none;" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+              <img src="{{ASSETS_PATH}}/assets/images/social-icons/x-twitter.svg" alt="x">
             </a>
           </div>
         </td>
